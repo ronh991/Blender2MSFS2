c35e3726a73e89a7a649a5c4a04a46bf2ad5f2f7
Ron Haertel <in_house_it@outlook.com>
2022-07-07 19:24:59 -0400

    Development branch - Blender Ver 3.3
    Updated for Blender 3.3 - will not work on lower version due to node changes in blender. Don't use it in your active development cycle, as it adds variables and objects to the scene that may cause issues further down the line.",

--- io_scene_gltf2_msfs/blender/com/gltf2_blender_data_path.py
+++ io_scene_gltf2_msfs/blender/com/gltf2_blender_data_path.py
@@ -1,16 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
 # Copyright 2018-2021 The glTF-Blender-IO authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 
 def get_target_property_name(data_path: str) -> str:
@@ -40,3 +29,6 @@ def get_rotation_modes(target_property: str) -> str:
         return True, False, ["AXIS_ANGLE"]
     else:
         return False, False, []
+
+def is_bone_anim_channel(data_path: str) -> bool:
+    return data_path[:10] == "pose.bones"
